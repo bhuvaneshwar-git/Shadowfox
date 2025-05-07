dafcac409a80eb3afe8b864eb72e8da728392e76
bhuvaneshwar-git <blackbhuvaneshwar@gmail.com>
2025-05-07 11:22:51 +0530

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,26 @@
+# Task 1 variables 
+''' 1) Create a variable named pi and store the value 22/7 in it.
+Now check the data type of this variable.'''
+pi = 22/7
+print(type(pi))
+
+'''2) Create a variable called for and assign it a value 4. See what
+happens and find out the reason behind the behavior that you
+see.'''
+# for = 4
+
+# ans = '"SyntaxError: invalid syntax"
+
+'''3) Store the principal amount, rate of interest, and time in
+different variables and then calculate the Simple Interest for 3
+years. Formula: Simple Interest = P x R x T / 100 '''
+
+principal_amount = 10
+rate_of_interest = 5
+time = 3
+
+simple_interest = principal_amount * rate_of_interest * time /100
+
+print(simple_interest)
+
+
